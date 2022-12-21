2c7efcc70e2304249cb1c62c05424ce60b73d3e8
Salem Akowe <salemakowe@gmail.com>
2022-12-21 21:06:42 +0100

    .

--- spare/lib/screens/home.dart
+++ spare/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import "package:flutter/material.dart";
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    RichText(
+                      text: const TextSpan(
+                        text: "Good morning",
+                        children: [TextSpan(text: "Ugo")],
+                      ),
+                    )
+                    // Text("Good morning, "),
+                    // Text("Ugo"),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        items: const [
+          Icon(Icons.home),
+          Icon(Icons.account_balance_wallet_outlined),
+          Icon(Icons.send_outlined),
+          Icon(Icons.sms_outlined),
+        ],
+        animationCurve: Curves.easeInOut,
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
   }
 }
