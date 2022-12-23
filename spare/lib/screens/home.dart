9ddafadfc347edde1570b340c35de74deb35bba1
Salem Akowe <salemakowe@gmail.com>
2022-12-23 23:44:18 +0100

    home page

--- spare/lib/screens/home.dart
+++ spare/lib/screens/home.dart
@@ -16,7 +16,33 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [CircleAvatar()],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Good morning",
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [Text("Ugo")],
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
       bottomNavigationBar: CurvedNavigationBar(
         items: const [
           Icon(Icons.home),
