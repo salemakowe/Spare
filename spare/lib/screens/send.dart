af02c552085f08b3a4651dffa19e6f469f777b14
Salem Akowe <salemakowe@gmail.com>
2023-01-10 21:52:47 +0100

    pages

---
+++ spare/lib/screens/send.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class SendPage extends StatefulWidget {
+  const SendPage({super.key});
+
+  @override
+  State<SendPage> createState() => _SendPageState();
+}
+
+class _SendPageState extends State<SendPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
