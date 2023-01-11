36fe2323f7611f0563bc6b73f5c1e7a45aefcf7b
Salem Akowe <salemakowe@gmail.com>
2023-01-11 21:15:19 +0100

    .

--- spare/lib/screens/bodytab.dart
+++ spare/lib/screens/bodytab.dart
@@ -12,43 +12,48 @@ class BodyTab extends StatefulWidget {
 class _BodyTabState extends State<BodyTab> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DefaultTabController(
-          length: 2,
-          child: Column(
-            children: [
-              TabBar(
-                isScrollable: true,
-                indicatorColor: Kolors.mainColor,
-                labelColor: Kolors.mainColor,
-                labelStyle: GoogleFonts.inter(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-                tabs: [
-                  const Tab(
-                    text: "Services",
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      child: Column(
+        children: [
+          DefaultTabController(
+            length: 2,
+            child: Column(
+              children: [
+                TabBar(
+                  // isScrollable: true,
+                  indicatorColor: Kolors.mainColor,
+                  labelColor: Kolors.mainColor,
+                  labelStyle: GoogleFonts.inter(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
                   ),
-                  const Tab(text: "Transactions"),
-                  TabBarView(children: [
-                    Text("Send"),
-                    Text("Pay Bills"),
-                    Text("USSD"),
-                  ])
-                ],
-              ),
-            ],
+                  tabs: [
+                    const Tab(
+                      text: "Services",
+                    ),
+                    const Tab(text: "Transactions"),
+                    // TabBarView(
+                    //   children: [
+                    //     Text("Send"),
+                    //     Text("Pay Bills"),
+                    //     Text("USSD"),
+                    //   ],
+                    // )
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        // Container(
-        //   child: TabBarView(children: [
-        //     Text("Send"),
-        //     Text("Pay Bills"),
-        //     Text("USSD"),
-        //   ]),
-        // )
-      ],
+          // Container(
+          //   child: TabBarView(children: [
+          //     Text("Send"),
+          //     Text("Pay Bills"),
+          //     Text("USSD"),
+          //   ]),
+          // )
+        ],
+      ),
     );
   }
 }
