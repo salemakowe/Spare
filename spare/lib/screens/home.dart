88cf2283afeed5ab33d0f1162cfbe97fd33eda81
Salem Akowe <salemakowe@gmail.com>
2023-01-17 14:23:56 +0100

    cleard the home

--- spare/lib/screens/home.dart
+++ spare/lib/screens/home.dart
@@ -6,7 +6,6 @@ import 'package:spare/screens/history.dart';
 import 'package:spare/screens/more.dart';
 import 'package:spare/screens/notifications.dart';
 import 'package:spare/widgets/addmoney.dart';
-import 'package:spare/widgets/bottomnavbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,123 +25,61 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // const List _pages = <Widget>[
+    //   Icon(Icons.camera, size: 150),
+    //   Icon(Icons.chat),
+    //   Icon(Icons.message),
+    //   Icon(Icons.call),
+    //   Icon(Icons.logout),
+    // ];
+
+    // int _selectedIndex = 0;
+    // // for the nav bar selected index.
+    // // The selectedIndex holds the value of the currently selected item
+
+    // void _onItemtapped(int index) {
+    //   setState(() {
+    //     _selectedIndex = index;
+    //   });
+    // }
+
     return Scaffold(
-      //body
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //User's avatar
-                Row(
-                  children: [
-                    Column(
-                      children: const [CircleAvatar()],
-                    ),
-
-                    const SizedBox(width: 4),
-
-                    //Greetings and User's name
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Good morning,",
-                              style: GoogleFonts.inter(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            )
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Ugo",
-                              style: GoogleFonts.inter(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.amber,
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Add to wallet",
+                            style: GoogleFonts.inter(),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.add_circle_outlined,
+                              color: Kolors.mainColor,
                             ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-
-                //notification bell
-                Column(
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          semanticLabel: "Add wallet",
-                          Icons.add_circle_outlined,
-                          color: Kolors.mainColor,
-                        ))
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(height: 30),
-
-            //Container for the card
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.23,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Kolors.mainColor,
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "spare balance",
-                          style: GoogleFonts.inter(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Text(
-                          "N10,000",
-                          style: GoogleFonts.inter(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 15),
-
-            //Services and Transaction
-
-            const BodyTab(),
-          ],
+            ],
+          ),
         ),
       ),
-
-      //Bottom Navigation
-      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
